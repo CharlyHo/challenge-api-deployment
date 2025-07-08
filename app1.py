bf56fe5c164e5235887de9ab27b42e0782c6c8ac
kameni <kamaugust2005@yahoo.fr>
2025-07-08 14:17:04 +0200

    save progress before checkout

--- app1.py
+++ app1.py
@@ -7,24 +7,25 @@ app = FastAPI()
 
 class PropertyInput(BaseModel):
     area: int
-    property_type: str
-    subtype_of_property: str
+    property_type: int
+    subtype_of_property: int
     rooms_number: int
     zip_code: int
     land_area: Optional[int] = 0
     garden: Optional[bool] = False
     garden_area: Optional[int] = 0
-    equipped_kitchen: Optional[bool] = False
     full_address: Optional[str] = None
     swimming_pool: Optional[bool] = False
-    furnished: Optional[bool] = False
     open_fire: Optional[bool] = False
     terrace: Optional[bool] = False
     terrace_area: Optional[int] = 0
     facades_number: Optional[int] = None
     building_condition: int
     parking: Optional[bool] = False
-    epcScore: Optional[int] = 5
+    epcScore: int
+    heating_type: int  
+    flood_zone_type: int
+    kitchen_types: int  
 
 @app.post("/predict")
 def predict_price(data: PropertyInput):
@@ -37,17 +38,20 @@ def predict_price(data: PropertyInput):
         "land-area": data.land_area,
         "garden": data.garden,
         "garden-area": data.garden_area,
-        "equipped-kitchen": data.equipped_kitchen,
         "full-address": data.full_address,
         "swimming-pool": data.swimming_pool,
-        "furnished": data.furnished,
         "open-fire": data.open_fire,
         "terrace": data.terrace,
         "terrace-area": data.terrace_area,
         "facades-number": data.facades_number,
         "building condition": data.building_condition,
         "parking": data.parking,
-        "epcScore": data.epcScore
+        "epcScore": data.epcScore,
+        "heating type": data.heating_type,
+        "flood zone type": data.flood_zone_type,
+        "kitchen types": data.kitchen_types
     }
+
+    
     price = predict(input_dict)
     return {"predicted_price": price}
--- predict/prediction.py
+++ predict/prediction.py
@@ -4,7 +4,7 @@ import pandas as pd
 from typing import Dict, Any
 
 # Load model
-model = joblib.load("model/poly_model.pkl")
+model = joblib.load("model/best_model.pkl")
 
 # Map text columns with the same encodings as in training
 
@@ -86,7 +86,7 @@ flood_zone_types = {
 
 kitchen_types = {
     -1: "Unknow",
-    0: 'NOT_INSTALLED,
+    0: 'NOT_INSTALLED',
     1: 'SEMI_EQUIPPED',
     2: 'INSTALLED',
     3: 'HYPER_EQUIPPED'
@@ -94,27 +94,31 @@ kitchen_types = {
 
 
 
-property_type_map = {
-    "APARTMENT": "APARTMENT",
-    "HOUSE": "HOUSE",
-    "OTHERS": "OTHER"
-}
+REQUIRED_FIELDS = [
+    "property-type",
+    "subtype of property",
+    "rooms-number",
+    "zip-code",
+    "area",
+    "land-area",
+    "garden-area",
+    "terrace",
+    "parking",
+    "Energy performance certificate score",
+    "heating type",
+    "flood zone type",
+    "building_condition",
+    "kitchen types"
+]
 
-subtype_map = {
-    "APARTMENT": "APARTMENT",
-    "DUPLEX": "DUPLEX",
-    "PENTHOUSE": "PENTHOUSE",
-    "STUDIO": "STUDIO",
-    "HOUSE": "HOUSE",
-    "VILLA": "VILLA",
-    "BUNGALOW": "BUNGALOW",
-    "MANSION": "MANSION",
-    "OTHER": "OTHER",
-    "MIXED_USE": "OTHER",
-    "FARMHOUSE": "FARMHOUSE",
-    "LOFT": "APARTMENT",
-    "TOWN_HOUSE": "HOUSE"
-}
+
+
+def check_missing_fields_strict(input_data: Dict[str, Any], expected_fields: list) -> None:
+    missing = [key for key in expected_fields if key not in input_data]
+    if missing:
+        print("[WARNING] mandatory missing field :")
+        for key in missing:
+            print(f" → '{key}' is mandatory but missing in the gave data.")
 
 
 
@@ -123,30 +127,27 @@ def predict(input_data: Dict[str, Any]) -> float:
     Takes a dictionary representing the preprocessed data
     of a property and returns the predicted price.
     """
+
+    check_missing_fields_strict(input_data, REQUIRED_FIELDS)
     
     # Prepare the data in the correct format (single-row DataFrame)
     processed_data = {
 
         "type": [property_type.get(input_data["property-type"])],
-        "subtype": [subtype_map.get(input_data["subtype of property"])],
+        "subtype": [subtype_mapping.get(input_data["subtype of property"])],
         "bedroomCount": [input_data["rooms-number"]],
-        "postCode": [str(input_data["postCode"])],  # province extraction
-        "subtype": [subtype_map.get(input_data["subtype of property"], "OTHER")],
-        "type": [property_type_map.get(input_data["property-type"], "OTHER")],
-        "bedroomCount": [input_data["rooms-number"]],
+        "postCode": [str(input_data["zip-code"])],  # province extraction
         "province": [str(input_data["zip-code"])[:2]],  # province extraction
         "habitableSurface": [input_data["area"]],
         "landArea": [input_data.get("land-area", 0)],
         "gardenSurface": [input_data.get("garden-area", 0)],
         "hasTerrace": [int(bool(input_data.get("terrace", False)))],
         "hasParking": [int(bool(input_data.get("parking", False)))],
-        "epc_score": [int(bool(input_data.get("terrace", False)))],
+        "epc_score": [input_data["epcScore"]],
+        "heatingType": [heating_types.get(input_data["heating type"])], 
+        "floodZoneTypes": [flood_zone_types.get(input_data["flood zone type"])], 
         "building_condition": [input_data["building condition"]],
-        "kitchen_types": [input_data.get["kitchen type"]]
-    }
-
-
-        "building_condition": [input_data["building condition"]]
+        "kitchenType": [kitchen_types.get(input_data["kitchen types"])] 
     }
 
     
@@ -161,8 +162,8 @@ def predict(input_data: Dict[str, Any]) -> float:
 if __name__ == "__main__":
     example_input = {
         "area": 120,
-        "property-type": "HOUSE",
-        "subtype of property": "HOUSE",
+        "property-type": 2,
+        "subtype of property": 2,
         "rooms-number": 3,
         "zip-code": 1020,
         "land-area": 300,
@@ -178,8 +179,13 @@ if __name__ == "__main__":
         "facades-number": 2,
         "building condition": 5,
         "parking": False,
-        "epcScore": 4
+        "epcScore": 4,
+        "heating type": 1,
+        "flood zone type": 1, 
+        "kitchen types": 3
+
     }
+
 
 
 
