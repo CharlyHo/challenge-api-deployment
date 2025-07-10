af1ebd2181bbc21e27691babef864d7d4a793066
Charly <hornaertcharly@gmail.com>
2025-07-10 13:59:16 +0200

    lastone

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ with st.form("prediction_form"):
     with col1:
         area = st.number_input("Habitable surface (m²)", min_value=10, step=1)
         rooms = st.number_input("Bedroom count", min_value=0, step=1)
-        subtype = st.selectbox("Subtype property", ["HOUSE", "VILLA", "APARTMENT", "STUDIO", "PENTHOUSE", "DUPLEX"])
+        subtype_encoded = st.selectbox("Subtype property", ["HOUSE", "VILLA", "APARTMENT", "STUDIO", "PENTHOUSE", "DUPLEX"])
         heatingType = st.selectbox("heating type", ["GAS", "ELECTRIC", "WOOD", "FUELOIL"])
         property_type = st.selectbox("Type property", ["HOUSE", "APARTMENT"])
         condition = st.selectbox("Building condition", [6, 5, 4, 3, 2, 1], format_func=lambda x: {
@@ -26,14 +26,14 @@ with st.form("prediction_form"):
 
 
     with col2:
-        zip_code = st.number_input("Postal code", min_value=1000, max_value=9999, step=1)
+        type_encoded = st.number_input("Postal code", min_value=1000, max_value=9999, step=1)
         land_area = st.number_input("Land surface (m²)", min_value=0, step=1)
         garden_area = st.number_input("Garden surface (m²)", min_value=0, step=1)
         terrace_area = st.number_input("Terrace surface (m²)", min_value=0, step=1)
         kitchenType = st.selectbox("kitchen types", ["NOT_INSTALLED", "SEMI_EQUIPPED", "INSTALLED", "HYPER_EQUIPPED"])
-        has_terrace = st.checkbox("Terrasse")
-        has_parking = st.checkbox("Parking")
-        epc_score = st.slider("Estimate epcScore (1=G ➜ 9=A++)", min_value=1, max_value=9, value=5)
+        hasTerrace_encoded = st.checkbox("Terrasse")
+        #has_parking = st.checkbox("Parking")
+        epcScore_encoded = st.slider("Estimate epcScore (1=G ➜ 9=A++)", min_value=1, max_value=9, value=5)
 
     submitted = st.form_submit_button("price prediction")
 
