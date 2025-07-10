76f61249594af0e83636fed391d59305665d03f0
Charly <hornaertcharly@gmail.com>
2025-07-10 14:04:41 +0200

    last

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,20 +19,20 @@ with st.form("prediction_form"):
             6: "AS_NEW", 5: "GOOD", 4: "JUST RENOVATED", 3: "TO_BE_DONE_UP", 2: "TO_RENOVATE", 1: "TO_RESTORE"
         }[x])
         floodZoneType = st.selectbox("flood zone type", ["NON FLOOD ZONE", "POSSIBLE FLOOD ZONE", "RECOGNIZED FLOOD ZONE"])
-        building_condition = st.selectbox("building condition", ["GOOD", "AS NEW", "TO RENOVATE", "TO BE DONE UP", 
-        "JUST RENOVATED", "TO RESTORE"])
+        #building_condition = st.selectbox("building condition", ["GOOD", "AS NEW", "TO RENOVATE", "TO BE DONE UP", 
+        #"JUST RENOVATED", "TO RESTORE"])
         bathRoom = st.number_input("Bathroom count", min_value=0, step=1)
         toilet = st.number_input("Toilet count", min_value=0, step=1)
 
 
     with col2:
-        type_encoded = st.number_input("Postal code", min_value=1000, max_value=9999, step=1)
+        zip_code = st.number_input("Postal code", min_value=1000, max_value=9999, step=1)
         land_area = st.number_input("Land surface (m²)", min_value=0, step=1)
         garden_area = st.number_input("Garden surface (m²)", min_value=0, step=1)
         terrace_area = st.number_input("Terrace surface (m²)", min_value=0, step=1)
         kitchenType = st.selectbox("kitchen types", ["NOT_INSTALLED", "SEMI_EQUIPPED", "INSTALLED", "HYPER_EQUIPPED"])
         hasTerrace_encoded = st.checkbox("Terrasse")
-        #has_parking = st.checkbox("Parking")
+        has_parking = st.checkbox("Parking")
         epcScore_encoded = st.slider("Estimate epcScore (1=G ➜ 9=A++)", min_value=1, max_value=9, value=5)
 
     submitted = st.form_submit_button("price prediction")
@@ -41,21 +41,21 @@ if submitted:
     input_data = {
         "area": area,
         "property-type": property_type,
-        "subtype of property": subtype,
+        "subtype of property": subtype_encoded,
         "rooms-number": rooms,
         "zip-code": zip_code,
         "kitchen types": kitchenType,
         "flood zone type": floodZoneType,
         "heating type": heatingType,
-        "Building condition": building_condition,
+        #"Building condition": building_condition,
         "land-area": land_area,
         "garden": garden_area > 0,
         "garden-area": garden_area,
-        "terrace": has_terrace,
-        "terrace-area": 10 if has_terrace else 0,
+        "terrace": hasTerrace_encoded,
+        "terrace-area": 10 if hasTerrace_encoded else 0,
         "building condition": condition,
         "parking": has_parking,
-        "epcScore": epc_score
+        "epcScore": epcScore_encoded
     }
 
     try:
